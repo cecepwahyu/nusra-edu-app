0057e6133a02105ad3eb12f431ec0691b8a395be
cecepwahyucahyana <cecepwahyu.2019@student.uny.ac.id>
2022-05-26 19:42:35 +0700

    first upload Nusra App

---
+++ Assets/code/game.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game : MonoBehaviour
+{
+    public void exit() {
+        Application.Quit();
+    }
+
+    public void back(string scene_name) {
+        Application.LoadLevel(scene_name);
+    }
+
+    public void telegram() {
+        Application.OpenURL("https://t.me/+7kWXwS_sEBI3M2U9");
+    }
+
+    public void video(string scene_name) {
+        Application.LoadLevel(scene_name);
+    }
+}
---
+++ Assets/code/videotarisumbar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class videotarisumbar : MonoBehaviour
+{
+    public void back(string scene_name) {
+        Application.LoadLevel(scene_name);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
